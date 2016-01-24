e730ebbc917846a27f61d9be5858e41bf01b2a16
ravi <rshri@gmail.com>
2016-01-24 18:24:48 +0530

    Added authentication

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,30 @@
-angular.module('flapperNews', ['ui.router', 'templates'])
+angular.module('flapperNews', ['ui.router', 'templates','Devise'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    })
     .state('home', {
       url: '/home',
       templateUrl: 'home/_home.html',
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require angular
 //= require angular-rails-templates
+//= require AngularDevise/lib/devise
 //= require angular-ui-router
 //= require_tree .
